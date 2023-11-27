34662eecbd6e386fb82a5585e10da19f1d2cfbff
Will Logan <willslogan@vt.edu>
2023-11-26 21:56:28 -0500

    Still needs some implementation but for the most part is finished

---
+++ Vocab+/Supporting Files/ImagePicker.swift
@@ -0,0 +1,19 @@
+//
+//  ImagePicker.swift
+//  Vocab+
+//
+//  Created by William Logan on 11/26/23.
+//  Copyright © 2023 IOS Team 7. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
